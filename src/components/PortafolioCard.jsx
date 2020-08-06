b2ec8729b3b5978a3136ad31f393b50d6ff89272
Pablo Ramirez <pablo@dreamsengine.io>
2020-08-06 17:25:49 -0500

    cards

--- src/components/PortafolioCard.jsx
+++ src/components/PortafolioCard.jsx
@@ -6,9 +6,11 @@ export default function PortafolioCard() {
     return (
         <div className="card">
             <img src="https://as.com/meristation/imagenes/2019/09/27/noticias/1569603388_605931_1569603425_noticia_normal.jpg" alt=""/>
-            <h3>Titulo</h3>
-            <p>Descripción</p>
-            <a href="#">Ver</a>
+            <div className="card-info">
+                <h3>Titulo</h3>
+                <p>Descripción</p>
+                <a href="#">Ver</a>
+            </div>
         </div>
     )
 }
